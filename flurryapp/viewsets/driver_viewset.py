3237f1c425624b7452026b6e7e06fe917e99ffd3
Mor Zusman <morzusman@gmail.com>
2016-03-14 14:51:00 +0200

    Documents

--- flurryapp/viewsets/driver_viewset.py
+++ flurryapp/viewsets/driver_viewset.py
@@ -5,5 +5,11 @@ from flurryapp.serializers.driver_serializer import DriverSerializer
 
 
 class DriverViewSet(viewsets.ModelViewSet, mixins.NestedViewSetMixin):
+    """
+    ViewSet for DataDriver 
+    NestedViewSetMixin is an Extension for DRF - makes possible to send simple
+        post/get requests.
+    """
+
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
--- flurryapp/viewsets/profile_viewset.py
+++ flurryapp/viewsets/profile_viewset.py
@@ -5,5 +5,11 @@ from flurryapp.serializers.profile_serializer import ProfileSerializer
 
 
 class ProfileViewSet(viewsets.ModelViewSet, mixins.NestedViewSetMixin):
+    """
+    ViewSet for DataDriver 
+    NestedViewSetMixin is an Extension for DRF - makes possible to send simple
+    post/get requests.
+    """
+
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
