31acb4803f591901aef092095dc5862f1eed9f46
Asaf <Asaf>
2016-03-24 22:50:37 +0200

    details route to append data to driver data

--- flurryapp/viewsets/driver_viewset.py
+++ flurryapp/viewsets/driver_viewset.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 from rest_framework_extensions import mixins
 from flurryapp.models import Driver
 from flurryapp.serializers.driver_serializer import DriverSerializer
@@ -13,3 +15,15 @@ class DriverViewSet(viewsets.ModelViewSet, mixins.NestedViewSetMixin):
 
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
+
+    @detail_route(methods=['POST'])
+    def insert_driving_data(self, request, *args, **kwargs):
+        driver = self.get_object()
+        driving_data_json = request.data
+        if isinstance(driving_data_json, dict):
+            print driving_data_json
+            driver.driving_data.data.append(driving_data_json)
+            driver.driving_data.save()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
--- flurryapp/zona.py
+++ flurryapp/zona.py
@@ -1,31 +1,33 @@
-import django
-django.setup()
-from flurryapp.models import *
+# import django
+# django.setup()
+# from flurryapp.models import *
+#
+# data_driver = DataDriver(data={
+#     'name': 'Mor',
+# }).save()
+#
+# print data_driver.data
+#
+# driver = Driver(name='Mor', driving_data=+data_driver).save()
+#
+# print driver
+#
+# car1 = Car(manufacturer='bla1', production_year=1995, model='blabka1', owner=driver).save()
+# car2 = Car(manufacturer='bla2', production_year=1995, model='blabka2', owner=driver).save()
+#
+# print car1
+# print car2
+#
+# profile = Profile(driver=driver).save()
+#
+# print profile
+#
+# print '\n\n~~~~~~~~~~~~~~~~~~\n\n'
+#
+# print driver.cars.all()
+#
+# print driver.profiles.all()
 
-data_driver = DataDriver(data={
-    'name': 'Mor',
-}).save()
 
-print data_driver.data
-
-driver = Driver(name='Mor', driving_data=data_driver).save()
-
-print driver
-
-car1 = Car(manufacturer='bla1', production_year=1995, model='blabka1', owner=driver).save()
-car2 = Car(manufacturer='bla2', production_year=1995, model='blabka2', owner=driver).save()
-
-print car1
-print car2
-
-profile = Profile(driver=driver).save()
-
-print profile
-
-print '\n\n~~~~~~~~~~~~~~~~~~\n\n'
-
-print driver.cars.all()
-
-print driver.profiles.all()
 
 
