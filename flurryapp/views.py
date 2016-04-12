ed6e05d36e9626c83a1107b4c5cc6d411c419d85
asaf <asafsemail@gmail.com>
2016-04-12 12:39:59 +0300

    landingpage

--- flurryapp/views.py
+++ flurryapp/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    html = '''
-    <p><h3><b>Flurry Project<b><h3><p>
 
-    <br><a href="api/v1/flurry/">API</a></br>
-    <br><a href="admin/">Admin Page</a></br>
-    '''
-    return HttpResponse(html)
+def index(request):
+    return render(request, 'landingpage.html')
