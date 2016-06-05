0c323303673ceb08025b0a6e99b27dc0fb543c19
asaf <asafsemail@gmail.com>
2016-06-05 12:45:48 +0300

    fixes

--- flurryapp/viewsets/data_driver_viewset.py
+++ flurryapp/viewsets/data_driver_viewset.py
@@ -5,6 +5,8 @@ from rest_framework_extensions import mixins
 from flurryapp.models import DataDriver
 from flurryapp.serializers.data_driver_serializer import DataDriverSerializer
 
+COLUMNS_TO_CONVERT = ['rpm', 'throttle', 'accelerator', 'rpm', 'speed']
+
 
 class DataDriverViewSet(viewsets.ModelViewSet, mixins.NestedViewSetMixin):
     """
@@ -18,5 +20,16 @@ class DataDriverViewSet(viewsets.ModelViewSet, mixins.NestedViewSetMixin):
     @detail_route(methods=['GET'])
     def merged(self, request, *args, **kwargs):
         driving_data = self.get_object()
-        data = reduce(lambda x, y: x + y, driving_data.data)
+        # data = reduce(lambda x, y: x + y, driving_data.data)
+        data = [self.__convert_data_unit_values_to_numeric_values(data_unit) for ride in driving_data.data for data_unit in ride]
         return Response(data, status=status.HTTP_200_OK)
+
+    def __convert_data_unit_values_to_numeric_values(self, data_unit):
+        new_data = dict()
+        for key, value in data_unit.iteritems():
+            if key in COLUMNS_TO_CONVERT:
+                new_data[key] = float(value)
+            else:
+                new_data[key] = value
+
+        return new_data
