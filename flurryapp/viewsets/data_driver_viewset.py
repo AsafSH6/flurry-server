d2212d3d03c0227b868b2b08af07285614c526e2
asaf <asafsemail@gmail.com>
2016-06-05 17:59:33 +0300

    routing to get specific data

--- flurryapp/viewsets/data_driver_viewset.py
+++ flurryapp/viewsets/data_driver_viewset.py
@@ -17,6 +17,16 @@ class DataDriverViewSet(viewsets.ModelViewSet, mixins.NestedViewSetMixin):
     queryset = DataDriver.objects.all()
     serializer_class = DataDriverSerializer
 
+    def retrieve(self, request, *args, **kwargs):
+        obj = self.get_object()
+        ride = request.query_params.get('ride', None)
+        data_unit = request.query_params.get('data_unit', None)
+        if ride is not None and len(obj) > 0:
+            data = obj[int(ride)][int(data_unit)] if data_unit is not None else obj[int(ride)]
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            return super(DataDriverViewSet, self).retrieve(request, *args, **kwargs)
+
     @detail_route(methods=['GET'])
     def merged(self, request, *args, **kwargs):
         driving_data = self.get_object()
@@ -25,6 +35,7 @@ class DataDriverViewSet(viewsets.ModelViewSet, mixins.NestedViewSetMixin):
             # data = [self.__convert_data_unit_values_to_numeric_values(data_unit) for ride in driving_data.data for data_unit in ride]
         else:
             data = driving_data.data
+
         return Response(data, status=status.HTTP_200_OK)
 
     def __convert_data_unit_values_to_numeric_values(self, data_unit):
